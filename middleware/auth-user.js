989d5c3500a5b188eeda1a68030a7a73ff7d4ece
karla <karla@getharvest.com>
2021-12-09 16:35:11 -0800

    Add auth-user handler

---
+++ middleware/auth-user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const auth = require('basic-auth');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+
+// Middleware to authenticate the request using Basic Authentication
+exports.authenticateUser = async (req, res, next) => {
+  // Store the message to display later
+  let message;
+
+  const credentials = auth(req);
+  if (credentials) {
+    const user = await User.findOne({ where: { emailAddress: credentials.name } });
+    if (user) {
+      const authenticated = bcrypt
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        console.log(`Authentication successful for user: ${user.emailAddress}`);
+        // Store the retrieved user object on the request object
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for user: ${user.emailAddress}`;
+      }
+    } else {
+      message = `User not found for user: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+
+  // If user authentication fails, return a response with a 401 Unauthorized HTTP status code
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied ' });
+  } else {
+    next();
+  }
+};
